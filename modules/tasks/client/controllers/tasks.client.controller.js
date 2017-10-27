fbfb2801ce34b4bca06210d75954842a8b287261
jeukengl <lizjeukeng@gmail.com>
2017-10-26 22:57:47 -0400

    new changes in the tasks page

--- modules/tasks/client/controllers/tasks.client.controller.js
+++ modules/tasks/client/controllers/tasks.client.controller.js
@@ -49,5 +49,47 @@
         vm.error = res.data.message;
       }
     }
+
+    //My stuff below
+    //adds new post
+    $scope.posts = [
+
+    ];
+    //filtered posts declaration
+
+    $scope.filteredPosts = $scope.posts;
+
+    $scope.addPost = function(){
+      $scope.posts.push({name: $scope.name, type: $scope.type, level: $scope.level, priority: $scope.priority});
+      $scope.name = '';
+      $scope.type = '';
+      $scope.level = '';
+      $scope.priority = '';
+
+
+    }
+
+    //Returns the listing in the filtered posts array pointed to by the specified index
+
+    $scope.fListingAt = function(index){
+      return $scope.filteredPosts[index];
+    }
+
+    //Remove row function
+
+    $scope.removeRow = function(name){
+      var index = -1;
+      var comArr = eval( $scope.posts );
+      for( var i = 0; i < comArr.length; i++ ) {
+        if( comArr[i].name === name ) {
+          index = i;
+          break;
+        }
+      }
+      if( index === -1 ) {
+        alert( "Entry Deleted" );
+      }
+      $scope.posts.splice( index, 1 );
+    };
   }
 }());
