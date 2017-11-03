95022567ff96e79c0460a344bcc507bdac1e2252
sparkz90 <31346884+sparkz90@users.noreply.github.com>
2017-11-03 19:00:35 -0400

    Fixed a few minor errors I found

--- server/models/tasks.server.model.js
+++ server/models/tasks.server.model.js
@@ -2,51 +2,60 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-/**
- * Task Schema
- */
+/* Task Schema */
+
 var taskSchema = new Schema({
   title: {
     type: String,
     trim: true,
     required: 'Title is required',
   },
+  
   completionDate: {
     type: Date,
     trim: true,
     default: Date.now,
   },
+  
   taskType: {
     type: String,
     trim: true,
-    default: '',
+    default: ' ',
   },
+  
   skillLevel: {
     type: String,
     trim: true,
-    default: '',
+    default: ' ',
   },
+  
   taskType: {
     type: String,
     trim: true
   },
+  
   instructions: {
     type: String,
-    default: ''
+    default: ' '
   },
+  
   attachments: {
     type: String
   },
+  
   createdBy: {
     type: String
   },
+  
   updatedBy: {
     type: String
   },
+  
   updated: {
     type: Date
     default: Date
   },
+  
   created: {
     type: Date,
     default: Date.now
@@ -54,7 +63,7 @@ var taskSchema = new Schema({
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
-listingSchema.pre('save', function(next) {
+taskSchema.pre('save', function(next) {
   var currentTime = new Date;
   this.updated_at = currentTime;
   if(!this.created_at)
@@ -65,7 +74,7 @@ listingSchema.pre('save', function(next) {
 });
 
 /* Use your schema to instantiate a Mongoose model */
-var task = mongoose.model('task', listingSchema);
+var task = mongoose.model('task', taskSchema);
 
 /* Export the model to make it avaiable to other parts of your Node application */
 module.exports = task;
