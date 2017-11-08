83b113d186a3aba012752b2a441bebc97b5ba072
“darien” <darienufl@ufl.edu>
2017-11-07 22:14:57 -0500

    Added rest services call to the backend

--- modules/tasks/client/services/tasks.client.service.js
+++ modules/tasks/client/services/tasks.client.service.js
@@ -3,15 +3,60 @@
   'use strict';
 
   angular
-    .module('tasks')
-    .factory('TasksService', TasksService);
+    .module('tasks.services')
+    .factory('Occupation', Occupation);
+
+  Occupation.$inject = ['$window'];
+
+  function Occupation($window) {
+    var occup = {
+      task: $window.task
+    };
+
+    return occup;
+  }
 
   TasksService.$inject = ['$resource'];
 
   function TasksService($resource) {
-    return $resource('api/tasks/:taskId', {
-      taskId: '@_id'
-    }, {
+    var Tasks = $resource('/api/tasks', {}, {
+      update: {
+        method: 'PUT'
+      },
+      deleteProvider: {
+        method: 'DELETE',
+        url: '/api/tasks/listing',
+        params: {
+          provider: '@provider'
+        }
+      },
+      add: {
+        method: 'POST',
+        url: '/api/tasks/add'
+      }
+    });
+
+    angular.extend(Tasks, {
+      taskBuild: function (details) {
+        return this.add(details).$promise;
+      }
+    });
+
+    return Tasks;
+  }
+
+
+  angular
+      .module('tasks.services')
+      .factory('OccupationService', OccupationService);
+
+  OccupationService.$inject = ['$resource'];
+
+  function OccupationService($resource) {
+    return $resource('/api/tasks/:taskId', {
+      userId: '@_id'
+    },
+    {
       update: {
         method: 'PUT'
       }
