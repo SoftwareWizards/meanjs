76ff2e1539408e7e0f5c9e1f89bbbe720d9b8df7
jeukengl <lizjeukeng@gmail.com>
2017-11-08 16:16:53 -0500

    new working changes

--- modules/tasks/server/models/task.server.model.js
+++ modules/tasks/server/models/task.server.model.js
@@ -16,6 +16,24 @@ var TaskSchema = new Schema({
     required: 'Please fill Task name',
     trim: true
   },
+  type: {
+    type: String,
+    default: '',
+    required: 'Please fill Task type',
+    trim: true
+  },
+  level: {
+    type: String,
+    default: '',
+    required: 'Please fill Task level',
+    trim: true
+  },
+  priority: {
+    type: String,
+    default: '',
+    required: 'Please fill Task priority',
+    trim: true
+  },
   created: {
     type: Date,
     default: Date.now
