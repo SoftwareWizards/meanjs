30da85c48ab2db74d62db55c6af8b38e9faab0ed
jeukengl <lizjeukeng@gmail.com>
2017-11-13 20:09:48 -0500

    fixed list portion routing error

--- client/app/config/tasks.client.routes.js
+++ client/app/config/tasks.client.routes.js
@@ -12,22 +12,46 @@ angular.module('tasks').config(['$stateProvider', '$urlRouterProvider',
         templateUrl: 'app/views/list-tasks.client.view.html',
         params: {
           successMessage: null
+        },
+        //added data
+        data: {
+          //added this line
+          roles: ['user', 'admin','builder', 'climber'],
+
         }
       })
       .state('tasks.create', {
         url: '/create',
-        templateUrl: 'app/views/create-task.client.view.html'
+        templateUrl: 'app/views/create-task.client.view.html',
+        //added data
+        data: {
+          //added this line
+          roles: ['user', 'admin','builder', 'climber'],
+
+        }
       })
       .state('tasks.view', {
         url: '/:taskId',
-        templateUrl: 'app/views/view-task.client.view.html'
+        templateUrl: 'app/views/view-task.client.view.html',
+        //added data
+        data: {
+          //added this line
+          roles: ['user', 'admin','builder', 'climber'],
+
+        }
       })
       /*
         Create a state for editing an individual task.
        */
 	  .state('tasks.edit', {
 		url: '/edit/:taskId',
-		templateUrl: 'app/views/edit-task.client.view.html'
+		templateUrl: 'app/views/edit-task.client.view.html',
+      //added data
+      data: {
+        //added this line
+        roles: ['user', 'admin','builder', 'climber'],
+
+      }
 	  })
   }
 ]);
