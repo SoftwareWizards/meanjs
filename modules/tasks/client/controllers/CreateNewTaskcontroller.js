8de9994553933a4cb66b7ccf57d90520b0fb7d6e
“darien” <darienufl@ufl.edu>
2017-11-29 20:57:10 -0500

    Finally got the payment button to render; sing angular's version of js onload. Also fixed the controller configuration

--- modules/tasks/client/controllers/CreateNewTaskcontroller.js
+++ modules/tasks/client/controllers/CreateNewTaskcontroller.js
@@ -1,7 +1,17 @@
-app.controller('MainCtrl',['$scope',
-function($scope, $window){
+(function () {
+  'use strict';
 
-	//adds new post
+  // Tasks controller
+  angular
+    .module('tasks')
+    .controller('TasksController', TasksController);
+
+  TasksController.$inject = ['$scope', '$state', '$window'];
+
+  function TasksController ($scope, $state, $window, task) {
+    var vm = this;
+
+    //adds new post
 	$scope.posts = [
 
 		];
@@ -22,7 +32,7 @@ function($scope, $window){
   //Returns the listing in the filtered posts array pointed to by the specified index
 
   $scope.fListingAt = function(index){
-    return $scope.filteredPosts[index]; 
+    return $scope.filteredPosts[index];
   }
 
 	//Remove row function
@@ -41,4 +51,69 @@ function($scope, $window){
 		}
 		$scope.posts.splice( index, 1 );
 	};
-}]);
+
+    $scope.init = function () {
+    paypal.Button.render({
+
+      client: {
+        sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R', // from https://developer.paypal.com/developer/applications/
+        production: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'  // from https://developer.paypal.com/developer/applications/
+      },
+      env: 'sandbox', // sandbox Or 'production',
+
+      commit: true, // Show a 'Pay Now' button
+
+      style: {
+        label: 'pay',  // checkout | credit | pay | buynow | generic
+        size:   'responsive', // tiny | small | medium | large | responsive
+        color:  'silver',   // gold | blue | silver | black | orange
+        shape:  'pill'    // pill | rect
+      },
+
+      payment: function(data, actions) {
+        /*
+         * Set up the payment here
+         */
+
+        var compensation = document.getElementById('compensation').value;
+        return actions.payment.create({
+
+          transactions: [
+            {
+              amount: {
+                total: compensation,
+                currency: 'USD'
+              }
+            }
+          ]
+        });
+      },
+
+      onAuthorize: function(data, actions) {
+        /*
+         * Execute the payment here
+         */
+        return actions.payment.execute().then(function(response) {
+          console.log('The payment was completed!');
+          window.alert('Payment Complete!');
+        });
+      },
+
+      onCancel: function(data, actions) {
+        /*
+         * Buyer cancelled the payment
+         */
+        console.log('The payment was cancelled!');
+      },
+
+      onError: function(err) {
+        /*
+         * An error occurred during the transaction
+         */
+        console.log('The payment failed!');
+      }
+
+    }, '#paypal-button');
+  };
+  }
+}());
