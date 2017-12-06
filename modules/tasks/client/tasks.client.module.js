5c86a5fd21cf35e79832c74b1a1cf9380ee6eb0f
“darien” <darienufl@ufl.edu>
2017-12-06 00:58:17 -0500

    Properly registered the routes and services to the task module

--- modules/tasks/client/tasks.client.module.js
+++ modules/tasks/client/tasks.client.module.js
@@ -1,5 +1,7 @@
 (function (app) {
   'use strict';
 
-  app.registerModule('tasks');
+  app.registerModule('tasks', ['core']);// The core module is required for special route handling; see /core/client/config/core.client.routes
+  app.registerModule('tasks.services');
+  app.registerModule('tasks.routes', ['ui.router', 'core.routes', 'tasks.services']);
 }(ApplicationConfiguration));
